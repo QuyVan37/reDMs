29accedbb70833151a368014c32df325cc337cb4
QuyVan37 <75932619+QuyVan37@users.noreply.github.com>
2024-07-19 17:32:09 +0700

    Create MaHoaPASS.cs

---
+++ MaHoaPASS.cs
@@ -0,0 +1,190 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using System.Web.Routing;
+
+namespace DMS03.Helpers
+{
+    public static class MyHelpers
+    {
+        // Render BootStrap menu item with active class
+        public static MvcHtmlString MenuItem(this HtmlHelper htmlHelper,
+                                             string text, string action,
+                                             string controller,
+                                             object routeValues = null,
+                                             object htmlAttributes = null)
+        {
+            var li = new TagBuilder("li");
+            var routeData = htmlHelper.ViewContext.RouteData;
+            var currentAction = routeData.GetRequiredString("action");
+            var currentController = routeData.GetRequiredString("controller");
+            if (string.Equals(currentAction,
+                              action,
+                              StringComparison.OrdinalIgnoreCase) &&
+                string.Equals(currentController,
+                              controller,
+                              StringComparison.OrdinalIgnoreCase))
+            {
+                li.AddCssClass("active");
+            }
+            if (routeValues != null)
+            {
+                li.InnerHtml = (htmlAttributes != null)
+                    ? htmlHelper.ActionLink(text,
+                                            action,
+                                            controller,
+                                            routeValues,
+                                            htmlAttributes).ToHtmlString()
+                    : htmlHelper.ActionLink(text,
+                                            action,
+                                            controller,
+                                            routeValues).ToHtmlString();
+            }
+            else
+            {
+                li.InnerHtml = (htmlAttributes != null)
+                    ? htmlHelper.ActionLink(text,
+                                            action,
+                                            controller,
+                                            null,
+                                            htmlAttributes).ToHtmlString()
+                    : htmlHelper.ActionLink(text,
+                                            action,
+                                            controller).ToHtmlString();
+            }
+            return MvcHtmlString.Create(li.ToString());
+        }
+
+
+        // As the text the: "<span class='glyphicon glyphicon-plus'></span>" can be entered
+        public static MvcHtmlString NoEncodeActionLink(this HtmlHelper htmlHelper,
+                                             string text, string title, string action,
+                                             string controller,
+                                             object routeValues = null,
+                                             object htmlAttributes = null)
+        {
+            UrlHelper urlHelper = new UrlHelper(htmlHelper.ViewContext.RequestContext);
+
+            TagBuilder builder = new TagBuilder("a");
+            builder.InnerHtml = text;
+            builder.Attributes["title"] = title;
+            builder.Attributes["href"] = urlHelper.Action(action, controller, routeValues);
+            builder.MergeAttributes(new RouteValueDictionary(HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes)));
+
+            return MvcHtmlString.Create(builder.ToString());
+        }
+        //generate random string
+        public static string RandomString(int size)
+        {
+            Random rnd = new Random();
+            string srds = "";
+            string[] str = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
+            for (int i = 0; i < size; i++)
+            {
+                srds = srds + str[rnd.Next(0, 61)];
+            }
+            return srds;
+        }
+
+        //encode password
+        public static string EncodePassword(string pass, string salt) //encrypt password    
+        {
+            byte[] bytes = Encoding.Unicode.GetBytes(pass);
+            byte[] src = Encoding.Unicode.GetBytes(salt);
+            byte[] dst = new byte[src.Length + bytes.Length];
+            System.Buffer.BlockCopy(src, 0, dst, 0, src.Length);
+            System.Buffer.BlockCopy(bytes, 0, dst, src.Length, bytes.Length);
+            HashAlgorithm algorithm = HashAlgorithm.Create("SHA1");
+            byte[] inArray = algorithm.ComputeHash(dst);
+            //return Convert.ToBase64String(inArray);    
+            return EncodePasswordMd5(Convert.ToBase64String(inArray));
+        }
+
+        //DMS danng dung ham nay
+        public static string EncodePasswordMd5(string pass) //Encrypt using MD5    
+        {
+            Byte[] originalBytes;
+            Byte[] encodedBytes;
+            MD5 md5;
+            //Instantiate MD5CryptoServiceProvider, get bytes for original password and compute hash (encoded password)    
+            md5 = new MD5CryptoServiceProvider();
+            originalBytes = ASCIIEncoding.Default.GetBytes(pass);
+            encodedBytes = md5.ComputeHash(originalBytes);
+            //Convert encoded bytes back to a 'readable' string    
+            return BitConverter.ToString(encodedBytes);
+        }
+        public static string base64Encode(string sData) // Encode    
+        {
+            try
+            {
+                byte[] encData_byte = new byte[sData.Length];
+                encData_byte = System.Text.Encoding.UTF8.GetBytes(sData);
+                string encodedData = Convert.ToBase64String(encData_byte);
+                return encodedData;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error in base64Encode" + ex.Message);
+            }
+        }
+        public static string base64Decode(string sData) //Decode    
+        {
+            try
+            {
+                var encoder = new System.Text.UTF8Encoding();
+                System.Text.Decoder utf8Decode = encoder.GetDecoder();
+                byte[] todecodeByte = Convert.FromBase64String(sData);
+                int charCount = utf8Decode.GetCharCount(todecodeByte, 0, todecodeByte.Length);
+                char[] decodedChar = new char[charCount];
+                utf8Decode.GetChars(todecodeByte, 0, todecodeByte.Length, decodedChar, 0);
+                string result = new String(decodedChar);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error in base64Decode" + ex.Message);
+            }
+        }
+
+        public static string Encrypt(string plainText)
+        {
+            string key = "jdsg432387#";
+            byte[] EncryptKey = { };
+            byte[] IV = { 55, 34, 87, 64, 87, 195, 54, 21 };
+            EncryptKey = System.Text.Encoding.UTF8.GetBytes(key.Substring(0, 8));
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            byte[] inputByte = Encoding.UTF8.GetBytes(plainText);
+            MemoryStream mStream = new MemoryStream();
+            CryptoStream cStream = new CryptoStream(mStream, des.CreateEncryptor(EncryptKey, IV), CryptoStreamMode.Write);
+            cStream.Write(inputByte, 0, inputByte.Length);
+            cStream.FlushFinalBlock();
+            return Convert.ToBase64String(mStream.ToArray());
+        }
+
+        public static string Decrypt(string encryptedText)
+        {
+            string key = "jdsg432387#";
+            byte[] DecryptKey = { };
+            byte[] IV = { 55, 34, 87, 64, 87, 195, 54, 21 };
+            byte[] inputByte = new byte[encryptedText.Length];
+
+            DecryptKey = System.Text.Encoding.UTF8.GetBytes(key.Substring(0, 8));
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            inputByte = Convert.FromBase64String(encryptedText);
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(DecryptKey, IV), CryptoStreamMode.Write);
+            cs.Write(inputByte, 0, inputByte.Length);
+            cs.FlushFinalBlock();
+            System.Text.Encoding encoding = System.Text.Encoding.UTF8;
+            return encoding.GetString(ms.ToArray());
+        }
+
+
+    }
+}
